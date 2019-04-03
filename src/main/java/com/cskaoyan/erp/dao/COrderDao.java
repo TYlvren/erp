ce103b4bdfa1508df48ad5e7693d0cd933320357
TYlvren <649729063@qq.com>
2019-04-03 16:52:40 +0800

    fix:service注解bug

--- src/main/java/com/cskaoyan/erp/dao/COrderDao.java
+++ src/main/java/com/cskaoyan/erp/dao/COrderDao.java
@@ -1,7 +1,17 @@
 package com.cskaoyan.erp.dao;
 
 
+import com.cskaoyan.erp.model.COrder;
+
+import java.util.List;
 
 public interface COrderDao {
+
+    /**
+     * 查询所有的客户订单
+     * @return
+     */
+    List<COrder> selectALLCOder();
+
 
 }
