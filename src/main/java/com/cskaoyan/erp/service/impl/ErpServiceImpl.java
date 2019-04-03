d5131ed33c7b5a9518f196bc097b4c001f85d347
TYlvren <649729063@qq.com>
2019-04-03 15:46:19 +0800

    feat：增加Service的模板

--- src/main/java/com/cskaoyan/erp/service/impl/ErpServiceImpl.java
+++ src/main/java/com/cskaoyan/erp/service/impl/ErpServiceImpl.java
@@ -1,7 +1,48 @@
 package com.cskaoyan.erp.service.impl;
 
+import com.cskaoyan.erp.dao.COrderDao;
+import com.cskaoyan.erp.model.COrder;
 import com.cskaoyan.erp.service.ErpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@RequestMapping("erpService")
 public class ErpServiceImpl implements ErpService {
+
+
+    /*****************计划进度接口实现*************************************/
+    @Autowired
+    private COrderDao cOrderDao;
+    /**
+     * 查询订单API的实现
+     * @see { ErpService }
+     * @return
+     */
+    @Override
+    public List<COrder> findCOrder() {
+        return null;
+    }
+
+
+    /*****************设备管理接口实现*************************************/
+
+
+    /*****************工艺监控接口实现*************************************/
+
+
+    /*****************物料监控接口实现*************************************/
+
+
+    /*****************质量监控接口实现*************************************/
+
+
+    /*****************人员监控接口实现*************************************/
+
+
+
+
+    /*****************系统管理接口实现*************************************/
 
 }
