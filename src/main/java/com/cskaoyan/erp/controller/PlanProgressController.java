7e29ad25d9d6817f06897c8025da78a08d0dda75
TYlvren <649729063@qq.com>
2019-04-04 09:45:19 +0800

    fix：解决了控制层的冲突

--- src/main/java/com/cskaoyan/erp/controller/PlanProgressController.java
+++ src/main/java/com/cskaoyan/erp/controller/PlanProgressController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -104,8 +105,8 @@ public class PlanProgressController {
     }
 
     @RequestMapping("product/insert")
-    public String insertProduct(){
-
+    public String insertProduct(@RequestBody Product product){
+        int i = erpService.addProduct(product);
         return "forward:list";
     }
 }
