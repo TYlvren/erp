89bcb3fecd38329e589507dea8dffdb4251a73cb
elephant4j <1051282115@qq.com>
2019-04-04 14:20:39 +0800

    fix:完善前端，给代码添加注释

--- src/main/java/com/cskaoyan/erp/controller/DeviceController.java
+++ src/main/java/com/cskaoyan/erp/controller/DeviceController.java
@@ -152,7 +152,7 @@ public class DeviceController {
      */
     @RequestMapping("deviceType/update")
     public @ResponseBody Map<String, String> update(DeviceType deviceType) {
-        System.out.println(deviceType);
+        //System.out.println(deviceType);
         Map<String, String> map = new HashMap<>();
         int i = erpService.updateDeviceType(deviceType);
         if (i > 0) {
@@ -163,9 +163,24 @@ public class DeviceController {
         return map;
     }
 
+    /**
+     * 对前端的请求进行映射，批量删除设备类别信息
+     * @return
+     */
+    @RequestMapping("deviceType/delete_judge")
+    public String delete1DeviceType() {
+        //System.out.println("dddddddddddd");
+        return "deviceType";
+    }
+
+    /**
+     * 这是一个批量删除设备类别的方法
+     * @param ids
+     * @return 删除成功则返回200状态码，失败则返回提示信息，删除失败
+     */
     @RequestMapping("deviceType/delete_batch")
     public @ResponseBody Map<String, String> delete(String ids) {
-        System.out.println(ids);
+        //System.out.println(ids);
         Map<String, String> map = new HashMap<>();
         String[] split = ids.split(",");
         int i = 0;
@@ -175,7 +190,7 @@ public class DeviceController {
         if (i > 0) {
             map.put("status", "200");
         } else {
-            map.put("msg", "修改失败");
+            map.put("msg", "删除失败");
         }
         return map;
     }
