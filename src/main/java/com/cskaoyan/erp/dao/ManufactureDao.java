943dd4c676ebf570087085a9d6a7ce27c4232884
TYlvren <649729063@qq.com>
2019-04-04 16:45:40 +0800

    feat：增加了work模块的查看功能

--- src/main/java/com/cskaoyan/erp/dao/ManufactureDao.java
+++ src/main/java/com/cskaoyan/erp/dao/ManufactureDao.java
@@ -1,7 +1,11 @@
 package com.cskaoyan.erp.dao;
 
 
+import com.cskaoyan.erp.model.Manufacture;
+
+import java.util.List;
 
 public interface ManufactureDao {
 
+    List<Manufacture> selectAllManufacture();
 }
--- src/main/java/com/cskaoyan/erp/dao/WorkDao.java
+++ src/main/java/com/cskaoyan/erp/dao/WorkDao.java
@@ -1,6 +1,11 @@
 package com.cskaoyan.erp.dao;
 
 
+import com.cskaoyan.erp.model.Work;
+
+import java.util.List;
+
 public interface WorkDao {
 
+    List<Work> selectAllWork();
 }
